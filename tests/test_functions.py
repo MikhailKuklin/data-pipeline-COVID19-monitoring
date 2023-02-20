a04472af674eadbbdbf67131ef82ffbd89a29942
Mikhail Kuklin <57541345+MikhailKuklin@users.noreply.github.com>
2023-02-20 20:00:44 +0200

    Update test_functions.py

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,23 +1,22 @@
-# test_my_script.py
 from src.web_to_gcs import *
 
 def test_fetch_exists():
     try:
-        func = getattr(web_to_gcs, fetch)
+        func = getattr(web_to_gcs, 'fetch')
     except AttributeError:
         func = None
     assert func is not None
 
 def test_clean_exists():
     try:
-        func = getattr(web_to_gcs, clean)
+        func = getattr(web_to_gcs, 'clean')
     except AttributeError:
         func = None
     assert func is not None
 
 def test_write_local_exists():
     try:
-        func = getattr(web_to_gcs, write_local)
+        func = getattr(web_to_gcs, 'write_local')
     except AttributeError:
         func = None
     assert func is not None
