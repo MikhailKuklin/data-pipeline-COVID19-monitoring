823d37654044c92da4b9adf2932a1ff8b11ebd09
Mikhail Kuklin <57541345+MikhailKuklin@users.noreply.github.com>
2023-02-20 21:17:12 +0200

    Update test_web_to_gcs_functions.py

--- tests/test_web_to_gcs_functions.py
+++ tests/test_web_to_gcs_functions.py
@@ -1,8 +1,6 @@
 # Check that required functions are exist in web_to_gcs code
 
-#from src.web_to_gcs import fetch, clean, write_local, write_gcs
-
-from src.web_to_gcs import *
+from src.web_to_gcs import fetch, clean, write_local, write_gcs
 
 def test_fetch_exists():
     assert fetch is not None
