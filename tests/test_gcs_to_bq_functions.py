9f82676ac17442d95298e57faab020086b644f27
Mikhail Kuklin <57541345+MikhailKuklin@users.noreply.github.com>
2023-02-20 20:14:12 +0200

    Create test_gcs_to_bq_functions.py

---
+++ tests/test_gcs_to_bq_functions.py
@@ -0,0 +1,15 @@
+# Check that required functions are exist in web_to_gcs code
+
+from src.gcs_to_bq import *
+
+def test_extract_from_gcs_exists():
+    assert extract_from_gcs is not None
+
+def test_transform_exists():
+    assert transform is not None
+
+def test_write_bq_exists():
+    assert write_bq is not None
+    
+def test_gcs_to_bq_exists():
+    assert gcs_to_bq is not None
