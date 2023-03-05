6c9664e188644c7cff4f4a28c6b5315398d47a70
MikhailKuklin <kuklin1990@gmail.com>
2023-03-05 10:40:15 +0000

    update script

--- scripts/setup_vm.sh
+++ scripts/setup_vm.sh
@@ -9,5 +9,8 @@ bash Anaconda3-2022.10-Linux-x86_64.sh
 # Download Terraform 1.3.9
 wget https://releases.hashicorp.com/terraform/1.3.9/terraform_1.3.9_linux_amd64.zip
 
+# Install unzip
+sudo apt install unzip
+
 # Unzip Terraform 1.3.9
 unzip terraform_1.3.9_linux_amd64.zip
