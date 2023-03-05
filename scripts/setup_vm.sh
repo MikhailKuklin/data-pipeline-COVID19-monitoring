8aafae6c23f4f7e3fdd6ccc9dd39e500b395f9cb
MikhailKuklin <kuklin1990@gmail.com>
2023-03-05 10:33:10 +0000

    add script with basic VM setups

---
+++ scripts/setup_vm.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Download Anaconda3-2022.10-Linux-x86_64.sh
+wget https://repo.anaconda.com/archive/Anaconda3-2022.10-Linux-x86_64.sh
+
+# Run Anaconda3-2022.10-Linux-x86_64.sh
+bash Anaconda3-2022.10-Linux-x86_64.sh
+
+# Download Terraform 1.3.9
+wget https://releases.hashicorp.com/terraform/1.3.9/terraform_1.3.9_linux_amd64.zip
+
+# Unzip Terraform 1.3.9
+unzip terraform_1.3.9_linux_amd64.zip
