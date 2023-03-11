e283b6c00cfa16e5d6ab34c6e76a31e2da9443c3
MikhailKuklin <kuklin1990@gmail.com>
2023-03-11 13:56:33 +0000

    add flow for dbt core

---
+++ src/run-dbt.py
@@ -0,0 +1,17 @@
+import os
+
+from prefect import flow
+from prefect_dbt.cli.commands import DbtCliProfile, DbtCoreOperation
+
+@flow(log_prints=True)
+def dbt_transform():
+    dbt_path = "../dbt_core"
+
+    dbt_op = DbtCoreOperation(
+        commands=["dbt debug", "dbt run --var 'is_test_run: false'"],
+        working_dir=dbt_path,
+        project_dir=dbt_path,
+        profiles_dir=dbt_path,
+    ).run()
+
+dbt_transform()
